d3472385aebb635489559cd7625917ef6b3af47e
Twin Panichsombat <roofimon@Twins-MacBook-Pro.local>
2013-03-07 15:09:02 +0700

    Initiate Homework

--- src/main/java/com/ing/life/account/controller/ListAllAccount.java
+++ src/main/java/com/ing/life/account/controller/ListAllAccount.java
@@ -16,6 +16,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.log4j.Logger;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 /**
  *
@@ -42,7 +44,9 @@ public class ListAllAccount extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // TODO Auto-generated method stub        
         logger.info("Start Fetch All Acclount in ListAllAccount");
-        AccountService accountService = new AccountHibernateImpl();
+        //AccountService accountService = new AccountHibernateImpl();
+        ApplicationContext beanFactory = WebApplicationContextUtils.getRequiredWebApplicationContext(getServletContext());
+        AccountService accountService = (AccountService)beanFactory.getBean("accountService");//new AccountHibernateImpl();
         List<Account> allAccount = accountService.getAllAccount();
         
         request.setAttribute("allAccount", allAccount);
